0cc8137d1cdc6cebc2e162a541dd122d7d47ef05
Matthew Wilt <msw9534@rit.edu>
2024-03-31 23:09:49 -0400

    finished domomaker B

--- server/controllers/Account.js
+++ server/controllers/Account.js
@@ -4,7 +4,10 @@ const { Account } = models;
 
 const loginPage = (req, res) => res.render('login');
 const signupPage = (req, res) => res.render('signup');
-const logout = (req, res) => res.redirect('/');
+const logout = (req, res) => {
+  req.session.destroy();
+  res.redirect('/');
+};
 
 const login = (req, res) => {
   const username = `${req.body.username}`;
@@ -18,6 +21,8 @@ const login = (req, res) => {
     if (err || !account) {
       return res.status(401).json({ error: 'Wrong username or password!' });
     }
+
+    req.session.account = Account.toAPI(account);
 
     return res.json({ redirect: '/maker' });
   });
@@ -40,6 +45,7 @@ const signup = async (req, res) => {
     const hash = await Account.generateHash(pass);
     const newAccount = new Account({ username, password: hash });
     await newAccount.save();
+    req.session.account = Account.toAPI(newAccount);
     return res.json({ redirect: '/maker' });
   } catch (err) {
     console.log(err);
